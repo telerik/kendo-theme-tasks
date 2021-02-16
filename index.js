808b6bbffe9c02015b58536bb119dcb8c7f17e8c
PreslavKozovski <preslav.kozovski@gmail.com>
2021-02-16 13:12:49 +0200

    feat(docs): add logic for generating utils docs

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const embedFile = require('./embedFile');
 const build = require('./build');
 const utils = require('./utils');
 const components = require('./components');
+const docs = require('./docs');
 
 module.exports.embedFile = embedFile;
 module.exports.build = build;
 module.exports.utils = utils;
 module.exports.components = components;
+module.exports.docs = docs;
