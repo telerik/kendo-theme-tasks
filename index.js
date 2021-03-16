74468e4ba7ed32bbf59c8dbcbbd0f335d9e28f63
PreslavKozovski <preslav.kozovski@gmail.com>
2021-03-16 17:43:21 +0200

    refactor: rename src/scss folder to src/build

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const embedFile = require('./embedFile');
-const sass = require('./sass');
+const build = require('./build');
 const utils = require('./utils');
 const components = require('./components');
 
 module.exports.embedFile = embedFile;
-module.exports.sass = sass;
+module.exports.build = build;
 module.exports.utils = utils;
 module.exports.components = components;
