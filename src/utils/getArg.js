a6cfd0106e87dbc26cf7c3461a9db48c18003819
joneff <joneff@gmail.com>
2021-07-26 15:41:41 +0300

    feat: add getEnvArg utility function

--- src/utils/getArg.js
+++ src/utils/getArg.js
@@ -6,4 +6,12 @@ function getArg(key, argsArr) {
     return (index < 0) ? null : (!next || next[0] === '-') ? true : next; // eslint-disable-line no-nested-ternary
 }
 
+function getEnvArg(key) {
+    let name = `npm_config_${key}`;
+    let arg = process.env[name];
+
+    return arg;
+}
+
 module.exports.getArg = getArg;
+module.exports.getEnvArg = getEnvArg;
