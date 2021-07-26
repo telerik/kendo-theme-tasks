81318edce472ff9989be922234608a02790a7480
joneff <joneff@gmail.com>
2021-07-26 15:41:41 +0300

    feat: add getEnvArg utility function

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,7 +3,7 @@ const { ensureDirSync } = require('./ensureDirSync');
 const { logger } = require('./logger');
 const { replacer } = require('./replacer');
 const { replacePathVariables } = require('./templatedPath');
-const { getArg } = require('./getArg');
+const { getArg, getEnvArg } = require('./getArg');
 
 module.exports.colors = colors;
 module.exports.ensureDirSync = ensureDirSync;
@@ -11,3 +11,4 @@ module.exports.logger = logger;
 module.exports.replacer = replacer;
 module.exports.replacePathVariables = replacePathVariables;
 module.exports.getArg = getArg;
+module.exports.getEnvArg = getEnvArg;
