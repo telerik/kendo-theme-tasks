7ac5b39b7ce7e88108d9557e4c689bccb41bea97
Juveniel <dimitar.kalaydzhievv@gmail.com>
2022-03-21 10:02:45 +0200

    feat(build): add kendo function wrappers for sass and json build

---
+++ src/build/kendo-build.js
@@ -0,0 +1,56 @@
+const { getDefaults } = require('./kendo-defaults');
+const { sassCompile, sassBuild } = require('./sass-build');
+const { jsonCompile, jsonBuild } = require('./json-build');
+const { kendoTransformer } = require('../../lib/jsonTransformers/kendo-transformer');
+
+const merge = require('lodash.merge');
+
+function kendoSassCompile( options ) {
+    const defaults = getDefaults({
+        cache: options.cache,
+        nodeModules: options.nodeModules
+    });
+    const buildOptions = merge( {}, defaults, options );
+
+    return sassCompile( buildOptions );
+}
+
+function kendoSassBuild( options ) {
+    const defaults = getDefaults({
+        cache: options.cache,
+        nodeModules: options.nodeModules
+    });
+    const buildOptions = merge( {}, defaults, options );
+
+    sassBuild( buildOptions );
+}
+
+function kendoJsonCompile( options ) {
+    const defaults = getDefaults({
+        cache: options.cache,
+        nodeModules: options.nodeModules
+    });
+    const buildOptions = merge( {}, defaults, options );
+
+    buildOptions.transformer = options.transformer || kendoTransformer;
+
+    return jsonCompile( buildOptions );
+}
+
+function kendoJsonBuild( options ) {
+    const defaults = getDefaults({
+        cache: options.cache,
+        nodeModules: options.nodeModules
+    });
+    const buildOptions = merge( {}, defaults, options );
+
+    buildOptions.transformer = options.transformer || kendoTransformer;
+
+    jsonBuild( buildOptions );
+}
+
+module.exports.kendoSassCompile = kendoSassCompile;
+module.exports.kendoSassBuild = kendoSassBuild;
+
+module.exports.kendoJsonCompile = kendoJsonCompile;
+module.exports.kendoJsonBuild = kendoJsonBuild;
