139e8d0baba06704ce29a02801e34845bff7af78
Juveniel <dimitar.kalaydzhievv@gmail.com>
2022-06-27 14:02:47 +0300

    fix: include dart template for create-component script

--- src/create/create-component.js
+++ src/create/create-component.js
@@ -4,14 +4,18 @@ const nunjucks = require('nunjucks');
 
 function createComponent(args) {
     const componentName = args.name;
-    const templatesDir = path.resolve(__dirname, 'templates/create-component');
-    const themes = [ 'default', 'bootstrap', 'material', 'classic' ];
-    const files = fs.readdirSync(templatesDir);
-
-    nunjucks.configure(templatesDir, { autoescape: false });
+    const templatesDir = path.resolve(__dirname, 'templates/create-component/node-sass');
+    const templatesDirDart = path.resolve(__dirname, 'templates/create-component/dart-sass');
+    const themes = [ 'default', 'bootstrap', 'material', 'classic', 'fluent' ];
 
     if (typeof componentName !== 'string') {
         throw 'Pass the component name as a parameter!';
+    }
+
+    function getTemplates(dir) {
+        nunjucks.configure(dir, { autoescape: false });
+
+        return fs.readdirSync(dir);
     }
 
     themes.forEach((theme) => {
@@ -20,13 +24,19 @@ function createComponent(args) {
             componentName: componentName,
             theme: theme
         };
+        const files = theme === 'fluent'
+            ? getTemplates(templatesDirDart)
+            : getTemplates(templatesDir);
 
         fs.mkdirSync(path.resolve(dist, componentName), { recursive: true });
 
         files.forEach((file) => {
             const output = nunjucks.render(file, data);
+            const outputFile = path.join(dist, componentName, path.basename(file, path.extname(file)));
 
-            fs.writeFileSync(path.join(dist, componentName, path.basename(file, path.extname(file))), output);
+            if (!fs.existsSync(outputFile)) {
+                fs.writeFileSync(outputFile, output);
+            }
         });
     });
 }
