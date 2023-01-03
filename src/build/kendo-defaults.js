eb64ad57a2f42d045cc735789dce871a690b38df
Dimitar Kalaydzhiev <dkalaydz@progress.com>
2023-01-03 13:40:21 +0200

    fix: pass filename in default build options

--- src/build/kendo-defaults.js
+++ src/build/kendo-defaults.js
@@ -1,7 +1,12 @@
 const { defaults } = require('sass-build/lib/sass-build-recommended');
 
+const output = {
+    path: 'dist',
+    filename: '[name].css',
+};
+
 function getDefaults() {
-    return defaults.build;
+    return Object.assign( {}, { output }, defaults.build );
 }
 
 module.exports.getDefaults = getDefaults;
